78bff3e9774e186ee7ce6d3b3bd168fbc78f284e
Arthur Ferdinand Lindner <arthur.lindner@outlook.de>
2019-12-20 15:45:55 -0500

    added user model

--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -4,10 +4,22 @@ export class User {
   lastname: string;
   password: string;
 
-  constructor(username: string, firstname: string, lastname: string, password: string) {
+  public All(username: string, firstname: string, lastname: string, password: string): User  {
     this.username = username;
     this.firstname = firstname;
     this.lastname = lastname;
     this.password = password;
+    return this;
+  }
+
+  public Login(username: string, password: string): User {
+    this.username = username;
+    this.password = password;
+    return this;
+  }
+
+  public User(username: string): User {
+    this.username = username;
+    return this;
   }
 }
