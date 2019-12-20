14c23390f8147c4b446461f9273c4e6ddac5570f
Arthur Ferdinand Lindner <arthur.lindner@outlook.de>
2019-12-20 15:45:50 -0500

    added media model

--- src/app/models/media.ts
+++ src/app/models/media.ts
@@ -2,6 +2,25 @@ export class Media {
   id: string;
   title: string;
   description: string;
+  creator: string;
+  timestamp: number;
+  url: string;
+  urlthumb: string;
   type: string;
   format: string;
+
+  public Upload(title: string, description: string, creator: string,
+                url: string, urlthumb: string, type: string,
+                format: string
+  ): Media {
+    this.title = title;
+    this.description = description;
+    this.creator = creator;
+    // this.timestamp = timestamp;
+    this.url = url;
+    this.urlthumb = urlthumb;
+    this.type = type;
+    this.format = format;
+    return this;
+  }
 }
