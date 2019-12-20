3c1379fd51dd32b8a956ba4c0687b7ca12379aa1
Arthur Ferdinand Lindner <arthur.lindner@outlook.de>
2019-12-20 15:45:16 -0500

    added user api interaction service

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 import { User } from '../models/user';
+import { observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -11,7 +12,14 @@ export class UserService {
   constructor(private httpClient: HttpClient) { }
 
   public createUser(user: User) {
-    console.log('Creating user' + user);
     return this.httpClient.post(environment.gateway + '/api/v1/user', user);
+  }
+
+  public loginUser(user: User) {
+    return this.httpClient.post(environment.gateway + '/api/v1/login', user, {observe: 'response', withCredentials: true});
+  }
+
+  public loggedIn(user: User) {
+    return this.httpClient.post(environment.gateway + '/api/v1/loggedin', user, {observe: 'response', withCredentials: true});
   }
 }
