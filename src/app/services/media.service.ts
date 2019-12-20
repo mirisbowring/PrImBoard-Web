b326ffb64ff2e60625a3729140f8b92b4bfc9a10
Arthur Ferdinand Lindner <arthur.lindner@outlook.de>
2019-12-20 15:45:04 -0500

    added media api interaction service

--- src/app/services/media.service.ts
+++ src/app/services/media.service.ts
@@ -1,7 +1,8 @@
 
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { environment } from '../../environments/environment';
+import { environment } from 'src/environments/environment';
+import { Media } from 'src/app/models/media';
 
 @Injectable({
   providedIn: 'root'
@@ -9,6 +10,10 @@ import { environment } from '../../environments/environment';
 export class MediaService {
 
   constructor(private httpClient: HttpClient) { }
+
+  public createMedia(media: Media) {
+    return this.httpClient.post(environment.gateway + '/api/v1/media', media, {observe: 'response', withCredentials: true});
+  }
 
   public getAllMedia() {
     return this.httpClient.get(environment.gateway + '/api/v1/media');
