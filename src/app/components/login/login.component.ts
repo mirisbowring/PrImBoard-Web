cc6498560f7dbe340061967b80130b9ec1d28a5b
Arthur Ferdinand Lindner <arthur.lindner@outlook.de>
2019-12-20 15:43:52 -0500

    added primitive login frontend

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+import { UserService } from 'src/app/services/user.service';
+import { User } from 'src/app/models/user';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +11,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginForm = new FormGroup({
+    username: new FormControl(''),
+    password: new FormControl('')
+  });
+
+  constructor(private userService: UserService, private router: Router) { }
 
   ngOnInit() {
+  }
+
+  submitlForm() {
+    this.userService.loginUser(
+      new User().Login(
+        this.loginForm.controls.username.value,
+        this.loginForm.controls.password.value
+      )
+    ).subscribe(
+      res => {
+        if (res.status === 200) {
+          localStorage.setItem('username', this.loginForm.controls.username.value);
+          this.router.navigate(['/home']);
+        }
+      },
+      err => {
+        console.log('Error:' + err);
+        localStorage.removeItem('username');
+      }
+    );
   }
 
 }
