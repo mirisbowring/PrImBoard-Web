3e9ad913d9086e4b478d628c3a1840ecbdf2db03
Arthur Ferdinand Lindner <arthur.lindner@outlook.de>
2019-12-20 15:43:08 -0500

    Added primitive upload frontend

--- src/app/components/upload/upload.component.ts
+++ src/app/components/upload/upload.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+import { MediaService } from 'src/app/services/media.service';
+import { Media } from 'src/app/models/media';
 
 @Component({
   selector: 'app-upload',
@@ -7,9 +10,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UploadComponent implements OnInit {
 
-  constructor() { }
+  public uForm = new FormGroup({
+    title: new FormControl(''),
+    description: new FormControl(''),
+    creator: new FormControl(''),
+    timestamp: new FormControl(''),
+    url: new FormControl(''),
+    urlThumb: new FormControl(''),
+    type: new FormControl(''),
+    format: new FormControl('')
+  });
+
+  constructor(private mediaService: MediaService) { }
+
+  types;
 
   ngOnInit() {
+    this.types = [
+      {prop: 'Video'},
+      {prop: 'Image'}
+    ];
+  }
+
+  private submituForm() {
+    this.mediaService.createMedia(new Media().Upload(
+      this.uForm.controls.title.value,
+      this.uForm.controls.description.value,
+      this.uForm.controls.creator.value,
+      // this.uForm.controls.timestamp.value,
+      this.uForm.controls.url.value,
+      this.uForm.controls.urlThumb.value,
+      this.uForm.controls.type.value,
+      this.uForm.controls.format.value
+    )).subscribe();
   }
 
 }
