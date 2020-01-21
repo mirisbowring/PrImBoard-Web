dde16990260c2d713152835b3569975c57dfe472
Arthur Ferdinand Lindner <arthur.lindner@outlook.de>
2020-01-21 06:52:16 -0500

    added comments to media

--- src/app/models/media.ts
+++ src/app/models/media.ts
@@ -1,4 +1,5 @@
 import { Tag } from 'src/app/models/tag';
+import { Comment } from 'src/app/models/comment';
 import { Event } from 'src/app/models/event';
 
 export interface Media {
@@ -6,6 +7,7 @@ export interface Media {
   sha1: string;
   title: string;
   description: string;
+  comments: Comment[];
   creator: string;
   tags: Tag[];
   events: string[];
