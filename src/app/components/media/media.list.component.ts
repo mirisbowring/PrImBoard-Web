2e0ebc7d727e99ffce8a38c718cc4f229e1246fd
Arthur Ferdinand Lindner <arthur.lindner@outlook.de>
2020-03-06 10:04:26 +0100

    Implemented auto destroy for subscriptions and support of multiple query/fragment types

--- src/app/components/media/media.list.component.ts
+++ src/app/components/media/media.list.component.ts
@@ -1,40 +1,71 @@
-import { Component, OnInit, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ViewChild, ElementRef, OnDestroy } from '@angular/core';
 import { MediaService } from 'src/app/services/media.service';
 import { Media } from 'src/app/models/media';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router, NavigationEnd } from '@angular/router';
+import { filter as fil, withLatestFrom } from 'rxjs/operators';
+import { DestroySubscribers, CombineSubscriptions } from 'ngx-destroy-subscribers';
+import { Unsubscribable } from 'rxjs';
 
 @Component({
   selector: 'app-media-list',
   templateUrl: './media.list.component.html',
   styleUrls: ['./media.list.component.css']
 })
-export class MediaListComponent implements OnInit, AfterViewInit {
+@DestroySubscribers()
+export class MediaListComponent implements OnInit, AfterViewInit, OnDestroy {
 
   @ViewChild('mediastream', { read: ElementRef, static: false }) public mediastream: ElementRef;
+
+  @CombineSubscriptions()
+  private subscribers: Unsubscribable;
 
   isFullListDisplayed = false;
   media: Media[] = [];
   filter: string;
+  prevID: string;
+  private curID: string;
 
-  constructor(private mediaService: MediaService, route: ActivatedRoute) {
-    route.params.subscribe(param => {
-      this.filter = param.filter;
-      this.mediaService.getMediaPage(null, 0, this.filter).subscribe((data: Media[]) => {this.media = data; this.requestMedia(); });
+  constructor(private mediaService: MediaService, route: ActivatedRoute, private router: Router) {
+    this.subscribers = this.router.events.pipe(
+      fil(e => e instanceof NavigationEnd),
+      withLatestFrom(route.params, route.fragment)
+    ).subscribe(([e, params, fragment]) => {
+      console.log(e instanceof NavigationEnd);
+      let operate = false;
+      if (this.curID !== fragment) {
+        this.curID = fragment;
+        this.prevID = fragment;
+        operate = true;
+      }
+      if (this.filter !== params.filter) {
+        this.filter = params.filter;
+        if (!operate || !this.curID) {
+          this.prevID = null;
+          operate = true;
+        }
+      }
+      if (operate || (e as NavigationEnd).urlAfterRedirects === '/home') {
+        console.log('getting');
+        this.media = [];
+        this.requestMedia();
+      }
     });
-   }
-
-  ngOnInit() {
   }
 
-  ngAfterViewInit() {
-  }
+  ngOnInit() { }
+
+  ngAfterViewInit() { }
+
+  ngOnDestroy() { }
 
   requestMedia() {
-    this.mediaService.getMediaPage(this.media[this.media.length - 1].id, 0, this.filter).subscribe((data: Media[]) => {
+    console.log(this.prevID + '     ' + this.curID + '    ' + this.filter);
+    this.mediaService.getMediaPage(this.prevID, 0, this.filter).subscribe((data: Media[]) => {
       if (data == null || data.length === 0) {
         this.isFullListDisplayed = true;
       } else {
         this.media = this.media.concat(data);
+        this.prevID = this.media[this.media.length - 1].id;
         this.callbackUntilScrollable();
       }
     });
@@ -49,7 +80,7 @@ export class MediaListComponent implements OnInit, AfterViewInit {
       if (this.mediastream.nativeElement.clientHeight <= window.innerHeight + 500) {
         this.onScroll();
       }
-    }, 0);  // If you use loading-placeholder.
+    }, 0);
   }
 
 
