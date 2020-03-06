3727cbc808b00a4625434c3352a78ddf0c0b2de7
Arthur Ferdinand Lindner <arthur.lindner@outlook.de>
2020-03-06 10:01:51 +0100

    redirect to last active item of list on back button pressed, implemented auto destroy for subscribers

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,53 +1,82 @@
-import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, OnDestroy } from '@angular/core';
 import { UserService } from 'src/app/services/user.service';
-import { Router } from '@angular/router';
-import { of, Observable } from 'rxjs';
+import { Router, NavigationStart } from '@angular/router';
+import { of, Observable, Unsubscribable } from 'rxjs';
 import { Tag } from 'src/app/models/tag';
 import { FormControl } from '@angular/forms';
-import { startWith, debounceTime, switchMap } from 'rxjs/operators';
+import { startWith, debounceTime, switchMap, filter } from 'rxjs/operators';
 import { TagService } from 'src/app/services/tag.service';
 import { AuthService } from 'src/app/services/auth.service';
+import { DestroySubscribers, CombineSubscriptions } from 'ngx-destroy-subscribers';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit, AfterViewInit {
+@DestroySubscribers()
+export class AppComponent implements OnInit, AfterViewInit, OnDestroy {
+
+  @CombineSubscriptions()
+  private subscribers: Unsubscribable;
 
   authenticated: boolean;
 
   tagAutoComplete$: Observable<Tag> = null;
   tagInput = new FormControl('');
 
-  constructor(private userService: UserService, public router: Router, private tagService: TagService, authService: AuthService) {
+  private id: string;
+  private filter: string;
 
-    // store authenticated in a local boolean to prevent delay due to cookie access
-    router.events.subscribe(val => {
-      this.authenticated = authService.isAuthenticated();
-      // parse filter from url if authenticated only
-      if (this.authenticated) {
-        // filter has to be manually parsed because this component is not in scope of router-outlet
-        let filter = window.location.pathname;
-        if (filter.startsWith('/home/')) {
-          // filter = filter.replace('/home/', '');
-          filter = filter.split('/')[2];
-          console.log(filter);
-          this.tagInput.setValue(filter);
-        } else {
-          this.tagInput.setValue('');
-        }
-      }
-    });
+  constructor(private userService: UserService, public router: Router, private tagService: TagService, private authService: AuthService) {
   }
 
   ngOnInit() {
+    // store authenticated in a local boolean to prevent delay due to cookie access
+    this.subscribers = this.router.events.subscribe(val => {
+      this.authenticated = this.authService.isAuthenticated();
+      // parse filter from url if authenticated only
+      if (this.authenticated) {
+        // filter has to be manually parsed because this component is not in scope of router-outlet
+        const path = window.location.pathname;
+        if (path.startsWith('/home/')) {
+          this.filter = path.split('/')[2];
+          this.tagInput.setValue(this.filter);
+        } else if (path.startsWith('/media/')) {
+          const paths = path.split('/');
+          this.filter = paths.length === 4 ? paths[2] : null;
+          this.id = paths.length === 4 ? paths[3] : paths[2];
+        } else {
+          this.tagInput.setValue('');
+          this.filter = null;
+        }
+      }
+    });
+
+    this.subscribers = this.router.events.pipe(
+      filter(event => event instanceof NavigationStart)
+    ).subscribe((event: NavigationStart) => {
+      if (event.restoredState) {
+        if (this.id && this.filter) {
+          this.router.navigate(['/home', this.filter], { fragment: this.id });
+          history.forward();
+        } else if (this.id) {
+          this.router.navigate(['/home'], { fragment: this.id });
+          history.forward();
+        } else if (this.filter) {
+          this.router.navigate(['/home', this.filter]);
+          history.forward();
+        }
+      }
+    });
   }
 
   ngAfterViewInit() {
     // pull tags
     this.receiveTags();
   }
+
+  ngOnDestroy() { }
 
   receiveTags() {
     this.tagAutoComplete$ = this.tagInput.valueChanges.pipe(
